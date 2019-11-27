632a45e647c502ec71b9fba325794e2c29e1317b
Dennis Morello <dennismorello@gmail.com>
2019-11-27 14:48:16 +0100

    Revised Google Analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: "Dennis Morello",
-    name: "Dennis Morello",
-    siteUrl: "https://morello.dev",
-    description: "A blog about web and mobile development by Dennis Morello.",
+    title: `Dennis Morello`,
+    name: `Dennis Morello`,
+    siteUrl: `https://morello.dev`,
+    description: `A blog about web and mobile development by Dennis Morello.`,
     hero: {
-      heading: "A blog about web and mobile development.",
+      heading: `A blog about web and mobile development.`,
       maxWidth: 652
     },
     social: [
       {
-        name: "twitter",
-        url: "https://twitter.com/dennismorello"
+        name: `twitter`,
+        url: `https://twitter.com/dennismorello`
       },
       {
-        name: "github",
-        url: "https://github.com/dennismorello"
+        name: `github`,
+        url: `https://github.com/dennismorello`
       },
       {
-        name: "instagram",
-        url: "https://instagram.com/dennismrl"
+        name: `instagram`,
+        url: `https://instagram.com/dennismrl`
       },
       {
-        name: "linkedin",
-        url: "https://www.linkedin.com/in/dennismorello"
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/dennismorello`
       }
     ]
   },
   plugins: [
     {
-      resolve: "@narative/gatsby-theme-novela",
+      resolve: `@narative/gatsby-theme-novela`,
       options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
+        contentPosts: `content/posts`,
+        contentAuthors: `content/authors`,
+        basePath: `/`,
         authorsPage: true,
         sources: {
           local: true
@@ -41,18 +41,18 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GA_TRACKID,
-        // Defines where to place the tracking script - "true" in the head and "false" in the body
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        exclude: [],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
@@ -60,25 +60,21 @@ module.exports = {
         // Enables Google Optimize Experiment ID
         experimentId: null,
         // Set Variation ID. 0 for original 1,2,3....
-        variationId: null,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "morello.dev"
+        variationId: null
       }
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Dev Blog",
-        short_name: "Dev Blog",
-        start_url: "/",
-        background_color: "#fff",
-        theme_color: "#fff",
-        display: "standalone",
-        icon: "src/assets/favicon.png"
+        name: `Dev Blog`,
+        short_name: `Dev Blog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
       }
     },
-    "gatsby-plugin-offline"
+    `gatsby-plugin-offline`
   ]
 };
